1dc5c187804212d69d389acc53e5dc6a2facb990
ArthurYang80 <342802865@tdsb.ca>
2026-03-06 02:54:44 +0000

    Finalized

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,33 @@
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
 	}
 
+	public static boolean validIndex(int[] array, int index) {
+		try {
+			return array[index] == array[index] || true;
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return false;
+		}
+	}
 	
+	public static int divide(int numerator, int denominator) {
+		try {
+			return numerator / denominator;
+		} catch (ArithmeticException e) {
+			return 0;
+		}
+	}
+	
+	public static int safeConvertStringtoInt(String str) {
+		try {
+			return Integer.parseInt(str);
+		} catch (NumberFormatException e) {
+			return 0;
+		}
+	}
 
 }
+
+
